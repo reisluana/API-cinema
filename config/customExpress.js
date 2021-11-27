dc2f09314048986233035df68f84ba73deb099e6
reisluana <reisluanag@gmail.com>
2021-11-27 02:54:01 -0300

    mysql install e remoção do body parser

--- config/customExpress.js
+++ config/customExpress.js
@@ -2,13 +2,17 @@
 
 const express = require('express')
 const consign = require('consign')
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
 
 module.exports = () => {
     const app = express()
-
-    app.use(bodyParser.urlencoded({extended: true}))
-    app.use(bodyParser.json)
+    
+   /* app.use(bodyParser.json)
+    app.use(bodyParser.urlencoded({extended: true}))*/
+    app.use(express.json())
+    app.use((req, res, next) => {
+        next()
+    })
 
     consign()
         .include('controllers')
